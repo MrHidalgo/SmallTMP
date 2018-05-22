dc5490fc29d93c73c28135ef9bf24d9b7de554ca
MrHidalgo <Luckyhidalgo@gmail.com>
2018-05-22 20:04:55 +0300

    Create task for js file.

---
+++ gulp/task/javascript.js
@@ -0,0 +1,49 @@
+const gulp          =   require('gulp'),
+  plumber           =   require('gulp-plumber'),
+  rename            =   require('gulp-rename'),
+  uglify            =   require('gulp-uglify'),
+  babel             =   require('gulp-babel'),
+  changedInPlace    =   require('gulp-changed-in-place');
+
+const config      = require('../config/config');
+
+const jsOption = {
+  plum: {
+    err: config.errorHandler
+  },
+  es6: {
+    "presets": ["env"]
+  },
+  renameOption: {
+    suffix : '.min'
+  },
+  changed: {
+    firstPass : true,
+    howToDetermineDifference: "modification-time"
+  }
+};
+
+const jsArr = {
+  0: [
+    config.src.js + '/*.js'
+  ],
+  1: [
+    config.src.js + '/**'
+  ]
+};
+
+gulp.task('js', function(e) {
+  return gulp
+    .src(jsArr[0])
+      .pipe(plumber(jsOption.plum.err))
+      .pipe(babel(jsOption.es6))
+      .pipe(changedInPlace(jsOption.changed))
+      .pipe(gulp.dest(config.dest.js))
+      .pipe(uglify())
+      .pipe(rename(jsOption.renameOption))
+      .pipe(gulp.dest(config.dest.js));
+});
+
+gulp.task('js:watch', function(e) {
+  gulp.watch(jsArr[1], ['js']);
+});

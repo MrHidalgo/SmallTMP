4c2f667fdef34211f01d37ebc660644233221055
MrHidalgo <Luckyhidalgo@gmail.com>
2018-05-23 14:43:26 +0300

    Fix clean task - add src img.

--- gulp/task/clean.js
+++ gulp/task/clean.js
@@ -6,7 +6,9 @@ const config = require('../config/config');
 gulp.task('clean', function() {
   return del.sync([
     config.dest.root + '/**/*',
+    config.src.root + '/img/**',
     '!' + config.dest.root + '/img',
-    '!' + config.dest.root + '/img/**/*'
+    '!' + config.dest.root + '/img/**/*',
+    '!' + config.src.root + '/img'
   ]);
 });

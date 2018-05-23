dbd2dd69963804cfede5b3491fcacb65ee36aca9
MrHidalgo <Luckyhidalgo@gmail.com>
2018-05-23 13:34:47 +0300

    Create server - browser-sync.

--- gulp/task/sever.js
+++ gulp/task/sever.js
@@ -1,2 +1,31 @@
 const gulp  = require('gulp'),
   server    = require('browser-sync').create();
+
+const config      = require('../config/config');
+
+gulp.task('server', function() {
+  server.init({
+    server: {
+      baseDir: config.dest.root,
+      directory: false,
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    },
+    files: [
+      config.dest.html + '/*.html',
+      config.dest.css + '/*.css',
+      config.dest.js + '/*.js',
+      config.dest.img + '/**/*'
+    ],
+    port: 3000,
+    logLevel: 'info', // 'debug', 'info', 'silent', 'warn'
+    logConnections: false,
+    logFileChanges: true,
+    open: true,
+    notify: false,
+    ghostMode: false,
+    online: true,
+    tunnel: false
+  });
+});

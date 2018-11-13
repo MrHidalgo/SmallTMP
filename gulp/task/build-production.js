01232b9213a06229f05718ad7ce329f566a4f2df
MrHidalgo <Luckyhidalgo@gmail.com>
2018-11-13 17:30:40 +0200

    Fix deploy production task.

--- gulp/task/build-production.js
+++ gulp/task/build-production.js
@@ -7,11 +7,7 @@ const gulp          = require('gulp'),
 	cssMinify         = require('gulp-cssmin'),
 	rename            = require('gulp-rename'),
 	stripCssComments  = require('gulp-strip-css-comments'),
-	mainBowerFiles  	= require('main-bower-files'),
-	scss              = require('gulp-sass'),
-	prefixer          = require('gulp-autoprefixer'),
 	uglify          	= require('gulp-uglify'),
-	babel           	= require('gulp-babel'),
   runSequence       = require('run-sequence');
 
 
@@ -27,41 +23,11 @@ const configPath  = require('../config/configPath'),
  * @description Gulp prodScript - build and concat source files to prod-ready mode.
  */
 gulp.task("prodScript", function() {
-	/**
-	 *
-	 * @type {string[]}
-	 */
-	const srcPathJs = [
-		configPath.src.js + '/*.js',
-		configPath.src.js + '/**/*.js',
-		'!' + configPath.src.js + '/**/_**.js',
-	];
-
-
-	let filesScript = mainBowerFiles('**/*.js');
-
-	filesScript.push(
-		configPath.src.vendorScript + "/*.js",
-		"!" + configPath.src.vendorScript + "/*.js",
-		configPath.src.vendorScript + "/**/*.js",
-		"!" + configPath.src.vendorScript + "/**/_**.js"
-	);
-
 	return gulp
-		.src(srcPathJs)
+		.src('dest/js/**')
 		.pipe(plumber(configOption.pipeBreaking.err))
 		.pipe(order([
-			"*",
-			"_lib/**",
-			"_window/**",
-			"_document/**",
-		]))
-		.pipe(concat('app.js'))
-		.pipe(babel(configOption.es6))
-		.pipe(gulp.src(filesScript))
-		.pipe(order([
-			'jquery.js',
-			'*'
+			"vendor.js"
 		]))
 		.pipe(concat('bundle.js'))
 		.pipe(uglify())
@@ -74,23 +40,11 @@ gulp.task("prodScript", function() {
  * @description Gulp prodStyle - build and concat source files to prod-ready mode.
  */
 gulp.task("prodStyle", function() {
-	let filesStyle = mainBowerFiles('**/*.css');
-
-	filesStyle.push(
-		configPath.src.vendorStyle + "/*.css",
-		configPath.src.vendorStyle + "/**/*.css",
-		"!" + configPath.src.vendorStyle + "/**/_**.css"
-	);
-
 	return gulp
-		.src(configPath.src.scss + '/*.scss')
+		.src('dest/css/**')
 			.pipe(plumber(configOption.pipeBreaking.err))
-			.pipe(scss(configOption.sassAPI).on('error', scss.logError))
-			.pipe(prefixer(configOption.autoPrefixOptions))
-			.pipe(gulp.src(filesStyle))
 			.pipe(order([
-				'normalize.css',
-				'*'
+				"vendor.css"
 			]))
 			.pipe(concat('bundle.css'))
 			.pipe(stripCssComments())
@@ -105,12 +59,10 @@ gulp.task("prodStyle", function() {
  */
 gulp.task("production", function() {
 	runSequence(
-		'clean',
+		'pug',
     [
-    	'pug',
 			'prodStyle',
 			'prodScript'
-		],
-		'list-pages'
+		]
   );
 });
